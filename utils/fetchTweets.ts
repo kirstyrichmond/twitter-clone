96d1844deca4eaafc3cd0dee6bf06bc47ce575f3
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-06-18 17:33:20 +0100

    .

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,7 +1,9 @@
 import { Tweet } from '../typings'
 
 export const fetchTweets = async () => {
-  const res = await fetch(`http://localhost:3000/api/getTweets`)
+  const res = await fetch(
+    `https://twitter-clone-gilt-two.vercel.app/api/getTweets`
+  )
   const data = await res?.json()
   const tweets: Tweet[] = data.tweets
   return tweets
