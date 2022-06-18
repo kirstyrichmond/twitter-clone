3193fa1c49958fe7b05d489a063cc5bf7a15140b
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-06-18 17:01:37 +0100

    .

--- sanity.js
+++ sanity.js
@@ -1,7 +1,9 @@
 import { createClient } from 'next-sanity'
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  dataset:
+    // process.env.NEXT_PUBLIC_SANITY_DATASET ||
+    'production',
   projectId: tn4ez646,
   apiVersion: '2021-10-21',
   useCdn: process.env.NODE_ENV === 'production',
